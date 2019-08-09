b1163194fe6f0b5b3df0b9ed24e72e3cc05f394c
Leonardo Cruz <leonardosamp11@gmail.com>
2019-08-09 17:17:41 -0300

    backend finalizado
    Implementados os controllers/models de Like/Dislike
    
    Adicionada a lib do cors
    
    rotas de like e dislike adicionadas

---
+++ backend/src/controllers/LikeController.js
@@ -0,0 +1,26 @@
+const DevModel = require('../models/DevModel')
+
+module.exports = {
+    async store(req, res) {
+        console.log(req.params.devId)
+        console.log(req.headers.user)
+
+        const { devId } = req.params
+        const { user } = req.headers
+
+        const loggedDev = await DevModel.findById(user)
+        const targetDev = await DevModel.findById(devId)
+
+        if (!targetDev) return res.status(400).json({ error: 'Dev not exists' })
+
+        if(targetDev.likes.includes(loggedDev._id)) {
+            console.log('Deu match')
+        }
+
+        loggedDev.likes.push(targetDev._id)
+
+        await loggedDev.save()
+
+        return res.json(loggedDev)
+    }
+}
