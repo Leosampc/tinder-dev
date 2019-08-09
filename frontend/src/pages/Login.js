65ae6007572ef8b1854598935b81d2844dfeb4b7
Leonardo Cruz <leonardosamp11@gmail.com>
2019-08-09 18:38:05 -0300

    Commit inicial frontend
    Criada estrutura do projeto;
    Pagina de login criada e estilizada

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Login.css'
+
+import logo from '../assets/logo.svg'
+
+export default function Login() {
+    return (
+        <div className="login-container">
+            <form>
+                <img src={logo} alt="Tindev" />
+                <input 
+                    type="text"
+                    placeholder="Digite seu usuario do Github"
+                />
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    )
+}
