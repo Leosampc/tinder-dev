429f641a03a330d6f36c122d61d0e9708ba76125
Leonardo Cruz <leonardosamp11@gmail.com>
2019-08-13 17:47:05 -0300

    Match front e correção do likeController
    Havia esquecido de implementar a logica de aviso do server para o front na controller de like, agora esta implementado;
    
    socket.io-client instalado no front e aviso de match implementado;

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -2,9 +2,6 @@ const DevModel = require('../models/DevModel')
 
 module.exports = {
     async store(req, res) {
-        console.log(req.params.devId)
-        console.log(req.headers.user)
-
         const { devId } = req.params
         const { user } = req.headers
 
@@ -14,7 +11,16 @@ module.exports = {
         if (!targetDev) return res.status(400).json({ error: 'Dev not exists' })
 
         if(targetDev.likes.includes(loggedDev._id)) {
-            console.log('Deu match')
+            const loggedSocket = req.connectedUsers[user]
+            const targetSocket = req.connectedUsers[devId]
+
+            if (loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
         loggedDev.likes.push(targetDev._id)
